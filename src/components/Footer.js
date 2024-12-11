0bc1d2738758ebb6945d1a3c1f24f70d65c580ae
Sanjulaarora <sanjulaarora3005@gmail.com>
2024-12-11 19:58:00 +0530

    made it better

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,13 +3,14 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer className='bg-[#66371e] text-white p-2'>
+
+      {/* div for desktop and tablet design */}
       <div className='hidden sm:block'>
         <div className='flex justify-around'>
           <div className='p-1 sm:p-4'>
             <h1 className='text-base sm:text-lg font-bold text-[#341c10]'>About Us</h1>
             <form className='w-96'>
-              <label htmlFor='email-footer' className='hidden sm:block'>Welcome to our website, a wonderful place to explore and learn how to cook like a pro.</label>
-              <label htmlFor='email-footer' className='block sm:hidden text-[12px] sm:text-base'>Welcome to our website.</label>
+              <label htmlFor='email-footer'>Welcome to our website, a wonderful place to explore and learn how to cook like a pro.</label>
               <input 
                 id='email-footer'
                 className='text-[10px] sm:text-base p-1 sm:p-2 w-24 sm:w-52 rounded-sm sm:rounded-lg'
@@ -30,13 +31,13 @@ const Footer = () => {
         </div>
       </div>
 
+      {/* div for mobile design */}
       <div className='block sm:hidden'>
         <div className='flex flex-col'>
           <div className='p-1 sm:p-4'>
             <h1 className='text-base sm:text-lg font-bold text-[#341c10]'>About Us</h1>
             <form className='w-96'>
-              <label className='hidden sm:block'>Welcome to our website, a wonderful place to explore and learn how to cook like a pro.</label>
-              <label className='block sm:hidden text-[12px] sm:text-base'>Welcome to our website.</label>
+              <label className='text-[12px] sm:text-base'>Welcome to our website.</label>
               <input 
                 className='text-[10px] sm:text-base p-1 sm:p-2 w-24 sm:w-52 rounded-sm sm:rounded-lg'
                 type='text'
